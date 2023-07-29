4cc23420dad04e0153902a911ac9f029b8e7580d
juancabeza <juancabeza@localhost>
2023-07-28 23:18:58 -0400

    Initial Commit

---
+++ src/dream/BucketList.java
@@ -0,0 +1,15 @@
+package dream;
+
+public class BucketList {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Juan's Bucket List:");
+		System.out.println("1. Visit the 7 wonders of modern world before my 30s");
+		System.out.println("2. Swim with white sharks");
+		System.out.println("3. Get a scuba diving license");
+		System.out.println("4. Go to a real African Safari");
+		System.out.println("5. Stay at the giraffe or elephants hotel");
+	}
+
+}
